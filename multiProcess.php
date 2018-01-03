b1798b4cb7820395001ca97c3aee1643ce61bb93
acai2046 <acai2046@sina.com>
2018-01-04 00:16:53 +0800

    Signed-off-by: acai2046 <acai2046@sina.com>

---
+++ multiProcess.php
@@ -0,0 +1,15 @@
+//multiProcess.php
+<?php 
+
+$pid = pcntl_fork();
+
+if ($pid) {
+    //父进程
+    echo "This is parent process\n";
+    pcntl_waitpid($pid, $status);
+} elseif ($pid == 0) {
+    //子进程
+    echo "This is child process\n";
+} else {
+    die("fork failed\n");
+}
