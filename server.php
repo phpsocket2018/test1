0dc06c09212727d2e525e9362378da8060b034ea
acai2046 <acai2046@sina.com>
2018-01-04 06:09:56 +0800

    Signed-off-by: acai2046 <acai2046@sina.com>

--- server.php
+++ server.php
@@ -1,6 +1,36 @@
 // server.php
 <?php 
 
+
+$sock = stream_socket_server("tcp://127.0.0.1:80", $errno, $errstr);
+
+$pids = [];
+
+for ($i=0; $i<10; $i++) {
+
+    $pid = pcntl_fork();
+    $pids[] = $pid;
+
+    if ($pid == 0) {
+        for ( ; ; ) {
+            $conn = stream_socket_accept($sock);
+
+            $write_buffer = "HTTP/1.0 200 OK\r\nServer: my_server\r\nContent-Type: text/html; charset=utf-8\r\n\r\nhello!world";
+
+            fwrite($conn, $write_buffer);
+
+            fclose($conn);
+        }
+
+        exit(0);
+    }
+
+}
+
+foreach ($pids as $pid) {
+    pcntl_waitpid($pid, $status);
+}
+/*
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
 socket_bind($sock, "127.0.0.1", 80);
@@ -15,4 +45,4 @@ for ( ; ; ) {
     socket_write($conn, $write_buffer);
 
     socket_close($conn);
-}
+}*/
